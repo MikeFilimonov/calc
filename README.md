f13f4461b9af8d1f8d8f430060720598ab5776f7
MikeFilimonov <filimonov.michael@gmail.com>
2022-11-16 17:24:08 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+"This calc operates with both Arabic and Roman numbers. Both operands should be of one kind, otherwise the app is terminated having thrown an exception.
+Same thing happens if the input is invalid due to either faulty operands or operators. The calc operates with ints in range from 1 to 10.
+The kind of input numbers defines the kind of the output.
+The quotient fractional part, is always omitted.
+The result of the calculation on the Arabic numbers can be positive, negative or of zero value.
+The result of the calculation on the Roman numbers can be only positive, otherwise the app throws an exception."
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String description = "This calc operates with both Arabic and Roman numbers." +
+                "Both operands should be of one kind, otherwise the app is terminated having thrown an exception." +
+                "Same thing happens if the input is invalid due to either faulty operands or operators." +
+                "The calc operates with ints in range from 1 to 10." +
+                "The kind of input numbers defines the kind of the output.";
+        System.out.println();
+
+        System.out.println(calc);
     }
+
+    public static String calc(String input){
+
+        String result = "";
+        System.out.println(result);
+    }
+
 }
